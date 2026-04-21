f3ff8f714456dca1de2685ae02ef1df2fb0df73f
Alexis Barrett <happynessb09@gmail.com>
2026-04-21 15:46:04 -0400

    Fixed a few errors and completed the challenge

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -14,18 +14,21 @@ public class Main {
         DateTimeFormatter num = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         DateTimeFormatter yearly = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter noMonth = DateTimeFormatter.ofPattern("EEE dd, yyyy");
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("EEE, dd MMM, yyyy HH:mm:ss 'GMT' ");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy HH:mm:ss 'GMT' ");
+        DateTimeFormatter chal = DateTimeFormatter.ofPattern("hh:mm 'on' dd-MMM-yyyy");
 
 
         String SlashDate = today.format(num);
         String yFirst = today.format(yearly);
         String NM = today.format(noMonth);
         String formattedDate = today.format(fmt);
+        String Challenge = today.format(chal);
 
 
         System.out.println(SlashDate);
         System.out.println(yFirst);
         System.out.println(NM);
         System.out.println(formattedDate);
+        System.out.println(Challenge);
     }
 }
