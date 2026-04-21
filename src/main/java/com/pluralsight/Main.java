2594ceab75ae0011e43e9ce55d05e7694152c9f9
Alexis Barrett <happynessb09@gmail.com>
2026-04-21 15:11:16 -0400

    getting the information for the date and time

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Getting the initial date and time.
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("Today is: " + today);
+    }
+}
